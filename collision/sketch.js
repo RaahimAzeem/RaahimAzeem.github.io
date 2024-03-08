9690aeb634f3046d9d829d36d997a95cc7c0e76b
RaahimAzeem <raahimazeem08@gmail.com>
2024-03-08 11:15:27 -0600

    Added Collide 2D Demo

---
+++ collision/sketch.js
@@ -0,0 +1,31 @@
+// Collide 2D Demo
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+let hit = false;
+
+function draw() {
+  background(255);
+  rect(200, 200, 100, 150);
+  circle(mouseX, mouseY, 100);
+
+  hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 100);
+  if (hit) {
+    stroke("red");
+  }
+  else {
+    stroke("black");
+
+  }
+
+  // Use vectors as input:
+  // const mouse      = createVector(mouseX, mouseY);
+  // const rect_start = createVector(200, 200);
+  // const rect_size  = createVector(100, 150);
+  // const radius     = 100;
+  // hit = collideRectCircleVector(rect_start, rect_size, mouse, radius);
+
+  
+  console.log('colliding?', hit);
+}
--- index.md
+++ index.md
@@ -3,6 +3,7 @@
 ## Demos 
 - [Traffic Light](Traffic Light)
 - [Bouncing Ball](bouncing_ball)
+- [Collision Demo](collision) 
 
 ## Projects 
 - [Interactive Scene](Interactive_Scene)
