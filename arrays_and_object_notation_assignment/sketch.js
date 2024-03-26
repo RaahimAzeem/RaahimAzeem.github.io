30ea72b4eb4b8639aae11efa8f3885e6786fb860
RaahimAzeem <raahimazeem08@gmail.com>
2024-03-26 14:05:36 -0600

    Little changes to the start screen

--- arrays_and_object_notation_assignment/sketch.js
+++ arrays_and_object_notation_assignment/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - 
 
-let state;
+let state = "startScreen";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,19 +14,25 @@ function setup() {
 function draw() {
   background(0);
   
-  displayTextOnStartup();
+  if (state === "startScreen") {
+    displayTextOnStartup();
+  }
+  else if (state === "game") {
+    
+  }
 }
 
 function displayTextOnStartup() {
+
   // Displaying text in white color which instructs how to start the game
   fill(255);
   textSize(30);
-  text("Press spacebar to start the game",width/2 - 150,height/2,);
+  text("Press spacebar to start the game",width/2,height/2);
+  text("The Alien Invasion Game", width/2, 50);
 
   // Displaying my name 
   fill(255);
   textSize(20);
   text("Made by Muhammad Raahim",width-300,height-50);
 }
-
 
