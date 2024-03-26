d015cdfb3eae9c56dc3698fb6d6b23fb3e91e4cc
RaahimAzeem <raahimazeem08@gmail.com>
2024-03-26 13:45:00 -0600

    Updating the Start Screen for Arrays and Object Notation Assignment

--- arrays_and_object_notation_assignment/sketch.js
+++ arrays_and_object_notation_assignment/sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - 
 
+let state;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,5 +13,20 @@ function setup() {
 
 function draw() {
   background(0);
+  
+  displayTextOnStartup();
 }
+
+function displayTextOnStartup() {
+  // Displaying text in white color which instructs how to start the game
+  fill(255);
+  textSize(30);
+  text("Press spacebar to start the game",width/2 - 150,height/2,);
+
+  // Displaying my name 
+  fill(255);
+  textSize(20);
+  text("Made by Muhammad Raahim",width-300,height-50);
+}
+
 
