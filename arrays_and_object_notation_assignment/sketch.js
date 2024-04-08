f9997d5750495fd4652bd30cad2df61774a5002e
RaahimAzeem <raahimazeem08@gmail.com>
2024-04-08 00:05:52 -0600

    Most of work done!

--- arrays_and_object_notation_assignment/sketch.js
+++ arrays_and_object_notation_assignment/sketch.js
@@ -8,32 +8,33 @@
 let state = "start screen";
 let alienArray = [];  
 let bulletArray = [];
-let alienToRemove;
-let bulletToRemove;
 let spaceship;
 let asteroid;
 let aliens;
+let gameLost;
+let gameWon;
 let shipX;
 let shipY;
 let shipDX = 10;
-let shipDY = 10;
 let sizeW = 75; 
 let sizeH = 75;
 let alienSpawnIntervalSet = false;
-let hit = false;
 let score = 0;
+let lives = 3;
 
 
 function preload() {
   spaceship = loadImage("spaceship.png");
   asteroid = loadImage("asteroid.png");
   aliens = loadImage("alien.png");
+  gameLost = loadImage("gameLost.jpg");
+  gameWon = loadImage("gameWon.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   shipX = width / 2;
-  shipY = height - 100;
+  shipY = height *9/10;
   noStroke();
 }
 
@@ -50,7 +51,10 @@ function gameScreen() {
   background(55);
   
   textSize(35);
-  text("Score: " + score, width-100, 40);
+  fill(255);
+  text("Score: " + score, width*9.3/10, height*0.5/9);
+  text("Lives: " + lives, width*9.3/10, height*1/8);
+
 
   // Spawning asteroids in the background to make it feel like space
   image(asteroid,random(width),random(height),20,20);
@@ -70,39 +74,34 @@ function gameScreen() {
   displayBullets();
   moveBullets();
   hitAlien();
+  gameResult();
   
 }
 
 function hitAlien() {
-  for (let theBullet of bulletArray) {
-    
-    for (let theAlien of alienArray) {
-      hit = collideRectRect(theBullet.bX,theBullet.bY,theBullet.bWidth,theBullet.bHeight,theAlien.alienX,theAlien.alienY,theAlien.alienW,theAlien.alienH);
-      
-      if (hit) {
-        theAlien = alienToRemove;
-        bulletArray.splice(bulletArray.indexOf(theBullet),1);
+  for (let theAlien of alienArray) {
+    for (let theBullet of bulletArray) {
+      if (collideRectRect(theBullet.bX, theBullet.bY, theBullet.bWidth, theBullet.bHeight,theAlien.alienX, theAlien.alienY, theAlien.alienW, theAlien.alienH)) {
         alienArray.splice(alienArray.indexOf(theAlien),1);
+        bulletArray.splice(bulletArray.indexOf(theBullet),1);
         score += 100;
       }
-
     }
-
   }
-
+  
 }
-// for (let i = bulletArray.length-1; i >= 0; i--) {
-//   if (hit) {
-//     bulletArray.splice(i,1);
-//     score += 100;
-//   }
-// }
 
-// for (let i = alienArray.length-1; i >= 0; i--) {
-//   if (hit) {
-//     alienArray.splice(i,1);
-//   }
-// }
+function gameResult() {
+  if (lives <= 0) {
+    background(55);
+    image(gameLost,width/5,0,1024,height);
+  }
+  if (score >= 1000) {
+    background(55);
+    image(gameWon,width/3,0,612,365);
+    
+  }
+}
 
 function keyPressed() {
   if (key === " ") {
@@ -140,6 +139,7 @@ function spawnBullets(bulletX) {
     bDY: 10,
     bWidth: 10,
     bHeight: 25,
+    color: "red",
   };
   bulletArray.push(bullet);
 }
@@ -147,6 +147,7 @@ function spawnBullets(bulletX) {
 function displayBullets() {
   // Looping through the bulletArray
   for (let theBullet of bulletArray) { 
+    fill(theBullet.color);
     rect(theBullet.bX, theBullet.bY, theBullet.bWidth,theBullet.bHeight);
   }
 }
@@ -159,7 +160,7 @@ function moveBullets() {
 
 function spawnAliens() {
   let alien = {
-    alienX: random(width-100),
+    alienX: random(width*8.5/10),
     alienY: 0, 
     alienDY: 2,
     alienW: 60,
@@ -178,7 +179,13 @@ function displayAliens() {
 function moveAliens() {
   for (let theAlien of alienArray) {
     theAlien.alienY += theAlien.alienDY;
+    if (theAlien.alienY > shipY) {
+      lives -= 1;
+      alienArray.splice(alienArray.indexOf(theAlien),1);
+    }
   }
+  
+  
 }
 
 function mousePressed() {
@@ -189,14 +196,63 @@ function mousePressed() {
 
 function displayTextOnStartup() {
   // Displaying text in white color which instructs how to start the game
+  fill("green");
+  textSize(105);
+  textStyle(BOLD);
+  text("SPACE INVADERS", width/2, height/6);
+
   fill(255);
-  textSize(42);
+  textSize(45);
   textAlign(CENTER, CENTER);
   text("Click the mouse to start the game",width / 2,height / 2);
   
 
   // Displaying my name 
   fill(255);
-  textSize(20);
-  text("Made by Muhammad Raahim",width-300,height-50);
+  textSize(30);
+  text("Use the Arrow Keys or the keys 'a' and 'd' to move the ship.",width/2,height*3/4);
+  text("Press the spacebar to fire bullets",width/2,height*3.3/4);
+  text("Press the key 'e' to play the easy mode, Press the key 'm' to play the medium mode, Press the key 'h' to play the hard mode",width/2,height*3.6/4);
+
 }
+ 
+// for (let i = bulletArray.length-1; i >= 0; i--) {
+//   if (hit) {
+//     bulletArray.splice(i,1);
+//     score += 100;
+//   }
+// }
+
+// for (let i = alienArray.length-1; i >= 0; i--) {
+//   if (hit) {
+//     alienArray.splice(i,1);
+//   }
+// }
+
+// Another way for alienHit() {
+// // Array to hold indices of bullets to be removed
+// let bulletsToRemove = [];
+ 
+// // First pass: identify bullets that hit an alien
+// for (let i = bulletArray.length - 1; i >= 0; i--) {
+//   let theBullet = bulletArray[i];
+//   for (let a = alienArray.length - 1; a >= 0; a--) {
+//     let theAlien = alienArray[a];
+//     if (collideRectRect(theBullet.bX, theBullet.bY, theBullet.bWidth, theBullet.bHeight,theAlien.alienX, theAlien.alienY, theAlien.alienW, theAlien.alienH)) {
+        
+//       // Mark the bullet for removal
+//       bulletsToRemove.push(i);
+        
+//       // Remove the alien
+//       alienArray.splice(a, 1);
+
+//       score += 100; // Increment the score
+//       break; // Exit the inner loop as the bullet has hit an alien
+//     }
+//   }
+// }
+ 
+// // Second pass: remove marked bullets
+// for (let i = bulletsToRemove.length - 1; i >= 0; i--) {
+//   bulletArray.splice(bulletsToRemove[i], 1);
+// }
